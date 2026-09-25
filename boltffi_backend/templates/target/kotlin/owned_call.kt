6eb797dd47108515b1ea7ca75592d87375db5a56
Ali A. Hilal <ali94_200970@yahoo.com>
2026-09-25 12:51:13 +0200

    simplify kotlin ownership cleanup

--- boltffi_backend/templates/target/kotlin/owned_call.kt
+++ boltffi_backend/templates/target/kotlin/owned_call.kt
@@ -5,16 +5,11 @@
     try {
 {% for argument in owned %}        {{ argument.local }} = {{ argument.parameter }}{% if argument.presence == HandlePresence::Nullable %}?.__boltffiTakeHandle() ?: 0L{% else %}.__boltffiTakeHandle(){% endif %}
 {% endfor %}{% for (name, value) in arguments %}        val {{ name }} = {{ value }}
-{% endfor %}        try {
-            val __boltffiResult = {{ invocation }}
-            __boltffiEntered = true
-            __boltffiResult
-        } catch (__boltffiFailure: UnsatisfiedLinkError) {
-            throw __boltffiFailure
-        } catch (__boltffiFailure: Throwable) {
-            __boltffiEntered = true
-            throw __boltffiFailure
-        }
+{% endfor %}        __boltffiEntered = true
+        {{ invocation }}
+    } catch (__boltffiFailure: UnsatisfiedLinkError) {
+        __boltffiEntered = false
+        throw __boltffiFailure
     } finally {
         if (!__boltffiEntered) {
 {% for argument in owned %}            if ({{ argument.local }} != 0L) Native.{{ argument.release }}({{ argument.local }})
